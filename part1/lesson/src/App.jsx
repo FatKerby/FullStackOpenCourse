73ac9f31d4507fde91e229c931e3b020b9f8cdc7
Kyle <242663018+FatKerby@users.noreply.github.com>
2025-11-08 07:33:16 -0500

    Completed Part 1a

---
+++ part1/lesson/src/App.jsx
@@ -0,0 +1,27 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Frida' age={23+4}/>
+      <Hello name={name} age={age}/>
+      <Hello />
+    </>
+  )
+}
+
+export default App
+
