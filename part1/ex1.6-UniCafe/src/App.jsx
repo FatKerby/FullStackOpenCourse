60866954957fa4b8ffd9cd812b1f3166121a1b32
Kyle <242663018+FatKerby@users.noreply.github.com>
2025-11-09 12:13:46 -0500

    completed ex1.9

--- part1/ex1.6-UniCafe/src/App.jsx
+++ part1/ex1.6-UniCafe/src/App.jsx
@@ -4,6 +4,13 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const Statistics = ({good, neutral, bad}) => {
   const total = good + neutral + bad
+  if (total === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
   return (
     <div>
       <div>good {good}</div>
