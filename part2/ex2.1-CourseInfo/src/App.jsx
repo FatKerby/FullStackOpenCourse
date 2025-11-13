c19115f5b65b4e7287ec71386304466081b9e51e
Kyle <242663018+FatKerby@users.noreply.github.com>
2025-11-12 19:44:12 -0500

    Added ex2.1

---
+++ part2/ex2.1-CourseInfo/src/App.jsx
@@ -0,0 +1,73 @@
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part.name} {props.part.exercises}</p>
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+    return (
+    <div>
+        <Header course={course} />
+        <Content course={course} />
+    </div>
+    )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  console.log(course);
+  console.log(course.name);
+  
+
+  return <Course course={course} />
+}
+
+export default App
