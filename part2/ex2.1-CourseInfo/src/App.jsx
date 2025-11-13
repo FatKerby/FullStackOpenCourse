afdbb42b3e9b74faa857cfde235e13f0c36cf207
Kyle <242663018+FatKerby@users.noreply.github.com>
2025-11-12 19:55:53 -0500

    Completed ex2.1

--- part2/ex2.1-CourseInfo/src/App.jsx
+++ part2/ex2.1-CourseInfo/src/App.jsx
@@ -9,9 +9,9 @@ const Header = ({ course }) => {
 const Content = ({ course }) => {
   return (
     <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      {course.parts.map((part, i) => 
+        <Part key={i} part={course.parts[i]} />
+      )}
     </div>
   )
 }
@@ -63,9 +63,6 @@ const App = () => {
       }
     ]
   }
-  console.log(course);
-  console.log(course.name);
-  
 
   return <Course course={course} />
 }
