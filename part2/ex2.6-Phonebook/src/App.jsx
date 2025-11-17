8da7267267315b8eaf17eb9261036cfdb3a6ce1f
Kyle <242663018+FatKerby@users.noreply.github.com>
2025-11-17 02:51:14 -0500

    Completed ex2.8

--- part2/ex2.6-Phonebook/src/App.jsx
+++ part2/ex2.6-Phonebook/src/App.jsx
@@ -4,18 +4,21 @@ import Person from './components/Person'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas',
+      number: '040-1234567',
       id: 'Arto Hellas',
     }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
     const isDuplicate = persons.some((person) => person.name === newName)
 
     if (!isDuplicate) {
       const nameObject = {
         name: newName,
+        number: newNumber,
         id: String(newName),
       }
       setPersons(persons.concat(nameObject))
@@ -24,23 +27,35 @@ const App = () => {
     }
 
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input 
           value={newName}
           onChange={handleNameChange}
           />
         </div>
-        <div>debug: {newName}</div>
+        <div>
+          number: <input
+          value={newNumber}
+          onChange={handleNumberChange}
+          />
+        </div>
+        <div>debug name: {newName}</div>
+        <div>debug number: {newNumber}</div>
         <div>
           <button type="submit">add</button>
         </div>
