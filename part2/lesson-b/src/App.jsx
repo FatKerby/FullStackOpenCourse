7969ad17373296a36d44bc8ae9b23005ccca04da
Kyle <242663018+FatKerby@users.noreply.github.com>
2025-11-18 03:33:40 -0500

    Completed lesson 2e

--- part2/lesson-b/src/App.jsx
+++ part2/lesson-b/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import Footer from './components/Footer'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
@@ -17,19 +19,22 @@ const App = () => {
   }, [])
 
   const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
+    const note = notes.find((n) => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
     noteService
       .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => note.id === id ? returnedNote : note))
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id === id ? returnedNote : note)))
       })
-      .catch(error => {
-        alert(
-          'the note "' + note.content + '" was already deleted from server'
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
-        setNotes(notes.filter(n => n.id !== id))
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter((n) => n.id !== id))
       })
   }
 
@@ -42,7 +47,7 @@ const App = () => {
     
     noteService
       .create(noteObject)
-      .then(returnedNote => {
+      .then((returnedNote) => {
         setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
@@ -54,11 +59,12 @@ const App = () => {
 
   const notesToShow = showAll
     ? notes
-    : notes.filter(note => note.important === true)
+    : notes.filter((note) => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -79,7 +85,8 @@ const App = () => {
           onChange={handleNoteChange}
         />
         <button type="submit">save</button>
-      </form>   
+      </form>
+      <Footer />
     </div>
   )
 }
