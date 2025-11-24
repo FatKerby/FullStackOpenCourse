1cc46b36016715690fc5efd9095e73ebc1afbda3
Kyle <242663018+FatKerby@users.noreply.github.com>
2025-11-23 20:26:47 -0500

    Completed ex3.16 - 3.18

--- part3/Phonebook_Backend/index.js
+++ part3/Phonebook_Backend/index.js
@@ -53,10 +53,16 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -67,10 +73,42 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))  
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
 app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people.</p>
+  response.send(`<p>Phonebook has info for ${Person.length} people.</p>
     <p>${new Date()}</p>`)
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
