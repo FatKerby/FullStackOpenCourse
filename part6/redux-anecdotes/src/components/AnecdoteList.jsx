f55ddd1adffda6b6cf7b83347542b8ef7e613df2
Kyle <242663018+FatKerby@users.noreply.github.com>
2025-11-27 22:06:09 -0500

    Completed ex6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
+
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <>
+      <div>{anecdote.content}</div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </>
+  )
+}
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  return (
+    <>
+      <h2>Anecdotes</h2>
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map(anecdote => (
+          <Anecdote
+            key={anecdote.id}
+            anecdote={anecdote}
+            handleClick={() => dispatch(vote(anecdote.id))}
+          />
+        ))
+      }
+    </>
+  )
+}
+
+export default AnecdoteList
