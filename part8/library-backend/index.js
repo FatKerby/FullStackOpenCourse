6d55a518ecbcdd8c89f49f01e69651d039335042
Kyle <242663018+FatKerby@users.noreply.github.com>
2026-02-21 01:23:21 -0500

    Add 8.6

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,6 +1,7 @@
 
-const { ApolloServer } = require('@apollo/server')
+const { ApolloServer, UserInputError } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -101,7 +102,7 @@ let books = [
 const typeDefs = /* GraphQL */`
   type Author {
     name: String!
-    born: Int!
+    born: Int
     bookCount: Int!
     id: ID!
   }
@@ -110,8 +111,8 @@ const typeDefs = /* GraphQL */`
     title: String!
     published: Int!
     author: String!
-    id: ID!
     genres: [String!]!
+    id: ID!
   }
 
   type Query {
@@ -119,6 +120,15 @@ const typeDefs = /* GraphQL */`
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
   }
 `
 
@@ -144,6 +154,23 @@ const resolvers = {
     bookCount: (root) => {
       const foundBooks = books.filter(book => book.author === root.name)
       return foundBooks.length
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+
+      if (books.find(book => book.title === args.title)) {
+        throw new UserInputError('Book already exists.', {
+          invalidArgs: args.title,
+        })
+      }
+
+      if (!authors.includes(book.author)) {
+        authors = authors.concat({ name: book.author, id: uuid() })
+      }
+      books = books.concat(book)
+      return book
     }
   }
 }
