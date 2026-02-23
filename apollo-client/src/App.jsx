90c38099a963c50b5b3903ccdc6103ff1608549c
Kyle <242663018+FatKerby@users.noreply.github.com>
2026-02-23 03:05:16 -0500

    Completed lesson 8d

--- apollo-client/src/App.jsx
+++ apollo-client/src/App.jsx
@@ -1,15 +1,17 @@
-import { useQuery } from '@apollo/client/react'
+import { useApolloClient, useQuery } from '@apollo/client/react'
 import { useState } from 'react'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import PhoneForm from './components/PhoneForm'
 import { ALL_PERSONS } from './queries'
 import Notify from './components/Notify'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('phonebook-user-token'))
   const [errorMessage, setErrorMessage] = useState(null)
-
   const result = useQuery(ALL_PERSONS)
+  const client = useApolloClient()
 
   if (result.loading) {
     return <div>loading...</div>
@@ -22,9 +24,29 @@ const App = () => {
     }, 10000)
   }
 
-  return  (
+  const onLogout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
+  }
+
+  return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={onLogout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify}/>
       <PhoneForm setError={notify} />
